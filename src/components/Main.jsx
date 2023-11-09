5c4fdfb9be476f73734c0219465554be8e0def89
Juan <jujotogon@gmail.com>
2023-11-09 02:06:45 -0500

    Pagina inicial y colores

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import StartPage from "../pages/StartPage.jsx";
+
+export default function Main() {
+  return (
+    
+    <NavigationContainer>
+      <StartPage />
+    </NavigationContainer>
+  );
+}
--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,13 +1,83 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+} from "react-native";
 
-export default function StartPage(){
-    return(
+export default function StartPage() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.title}>Brain Mentors</Text>
 
-        <View>
-            <Text>
-                Start page
-            </Text>
-        </View>
-    )
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => console.log("Ingresar")}
+        >
+          <Text style={styles.buttonText}>Ingresar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => console.log("Registrarse")}
+        >
+          <Text style={styles.buttonText}>Registrarse</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.bottomContainer}>
+        <TouchableOpacity onPress={() => console.log("Terms of Use")}>
+          <Text style={styles.bottomText}>Terms of Use</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => console.log("Privacy Policy")}>
+          <Text style={styles.bottomText}>Privacy Policy</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#D75177", // Color de fondo de la SafeAreaView
+  },
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20, // Espaciado superior para el título
+  },
+  title: {
+    fontSize: 50,
+    fontWeight: "bold",
+    marginBottom: 50,
+    color: "#F2F2F2"
+  },
+  button: {
+    backgroundColor: "#0CC0DF",
+    width: 200,
+    paddingVertical: 15,
+    borderRadius: 5,
+    marginVertical: 10,
+  },
+  buttonText: {
+    color: "#F2F2F2",
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  bottomContainer: {
+    alignItems: "center",
+    marginBottom: 20, // Espaciado inferior para los textos en la parte inferior
+  },
+  bottomText: {
+    color: "#F2F2F2",
+    fontSize: 16,
+    marginVertical: 5,
+  },
+});
